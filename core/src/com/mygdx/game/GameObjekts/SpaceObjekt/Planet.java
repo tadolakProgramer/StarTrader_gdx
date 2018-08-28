bea9360b0747c8eabec5e7eac704260526dbbe40
Tadeusz <tadolak@gmail.com>
2018-08-28 15:25:28 +0200

    modifity Planet construktor

--- core/src/com/mygdx/game/GameObjekts/SpaceObjekt/Planet.java
+++ core/src/com/mygdx/game/GameObjekts/SpaceObjekt/Planet.java
@@ -1,5 +1,6 @@
 package com.mygdx.game.GameObjekts.SpaceObjekt;
 
+import com.badlogic.gdx.graphics.Texture;
 import com.badlogic.gdx.scenes.scene2d.InputEvent;
 import com.badlogic.gdx.scenes.scene2d.utils.ClickListener;
 import com.mygdx.game.MyGdxGame;
@@ -9,7 +10,8 @@ import com.mygdx.game.MyGdxGame;
 public class Planet extends SpaceObject {
 
     public double planetScale;
-    private double priceCarbon;
+    private double priceTitan;
+    private double priceGrafen;
     private double priceWoter;
     private double priceFuell;
 
@@ -18,8 +20,24 @@ public class Planet extends SpaceObject {
         setScale(0.5f);
         setPosition(x,y);
         rotationSpeed =15;
+         addClickListener();
+    }
 
+    public Planet(final MyGdxGame game, float x, float y, Texture texture, String name, float speedRot, double priceT, double priceG, double priceW, double priceF) {
+        super(game);
+        setScale(0.5f);
+        setPosition(x,y);
+        rotationSpeed = speedRot;
+        setSpaceObjectName(name);
+        priceTitan = priceT;
+        priceGrafen = priceG;
+        priceWoter = priceW;
+        priceFuell = priceF;
+        setTexture(texture);
+        addClickListener();
+    }
 
+    private void addClickListener(){
         this.addListener(new ClickListener(){
             @Override
             public boolean touchDown(InputEvent event, float x, float y, int pointer, int button) {
