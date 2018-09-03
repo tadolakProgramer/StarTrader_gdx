6fe8107827fcf83fb4207e92adb2bdc779869baf
Tadeusz <tadolak@gmail.com>
2018-09-03 13:15:24 +0200

    clear space

---
+++ core/src/com/mygdx/game/Helper/CreateXmlFile.java
@@ -0,0 +1,117 @@
+package com.mygdx.game.Helper;
+
+
+import com.badlogic.gdx.Gdx;
+import com.badlogic.gdx.graphics.Texture;
+import com.badlogic.gdx.math.Vector2;
+import com.mygdx.game.GameObjekts.SpaceObjekt.Planet;
+import com.mygdx.game.MyGdxGame;
+
+import javax.xml.parsers.DocumentBuilderFactory;
+import javax.xml.parsers.DocumentBuilder;
+import javax.xml.transform.Transformer;
+import javax.xml.transform.TransformerFactory;
+import javax.xml.transform.dom.DOMSource;
+import javax.xml.transform.stream.StreamResult;
+import org.w3c.dom.Attr;
+import org.w3c.dom.CDATASection;
+import org.w3c.dom.Document;
+import org.w3c.dom.Element;
+import org.w3c.dom.Node;
+import org.w3c.dom.Text;
+
+import java.io.File;
+import java.util.ArrayList;
+import java.util.List;
+import java.util.Random;
+
+public class CreateXmlFile {
+
+
+    public static void crateSpace(MyGdxGame game) {
+
+        List<Planet> planets = new ArrayList<Planet>();
+
+        CreateSpace createSpace1 = new CreateSpace();
+
+        try {
+            DocumentBuilderFactory dbFactory =
+                    DocumentBuilderFactory.newInstance();
+            DocumentBuilder dBuilder = dbFactory.newDocumentBuilder();
+            Document doc = dBuilder.newDocument();
+
+            // root element
+            Element rootElement = doc.createElement("Planets");
+            doc.appendChild(rootElement);
+            planets = createSpace1.Create(game);
+
+            for (int z=1;z<=planets.size()-1;z++) {
+
+
+                            String path = planets.get(z).getPath();
+
+                            // supercars element
+                            Element planeta = doc.createElement("planet"+z);
+                            rootElement.appendChild(planeta);
+
+                            Node nanme = doc.createElement("name");
+                            nanme.appendChild(doc.createTextNode(planets.get(z).getSpaceObjectName()));
+                            planeta.appendChild(nanme);
+
+                            Node ntexture = doc.createElement("texture");
+                            ntexture.appendChild(doc.createTextNode(path));
+                            planeta.appendChild(ntexture);
+
+                            // Create a new Node with the given tag name
+                            Node nposx = doc.createElement("posx");
+                            float x = planets.get(z).getX();
+                            nposx.appendChild(doc.createTextNode(Float.toString(x)));
+                            planeta.appendChild(nposx);
+
+                            Node nposy = doc.createElement("posy");
+                            float y = planets.get(z).getY();
+                            nposy.appendChild(doc.createTextNode(Float.toString(y)));
+                            planeta.appendChild(nposy);
+
+                            Node nrot = doc.createElement("rot");
+                            float rRotation = planets.get(z).getRotationSpeed();
+                            nrot.appendChild(doc.createTextNode(Float.toString(rRotation)));
+                            planeta.appendChild(nrot);
+
+                Node nTitan = doc.createElement("Titan");
+                double rTitan = planets.get(z).getPriceTitan();
+                nTitan.appendChild(doc.createTextNode(Double.toString(rTitan)));
+                planeta.appendChild(nTitan);
+
+                Node nRgafen = doc.createElement("Grafen");
+                double rGrafen = planets.get(z).getPriceGrafen();
+                nRgafen.appendChild(doc.createTextNode(Double.toString(rGrafen)));
+                planeta.appendChild(nRgafen);
+
+                Node nWoter = doc.createElement("Woter");
+                double rWoter = planets.get(z).getPriceWoter();
+                nWoter.appendChild(doc.createTextNode(Double.toString(rWoter)));
+                planeta.appendChild(nWoter);
+
+                Node nFuell = doc.createElement("Fuell");
+                double rFuell = planets.get(z).getPriceFuell();
+                nFuell.appendChild(doc.createTextNode(Double.toString(rFuell)));
+                planeta.appendChild(nFuell);
+
+            }
+
+
+            // write the content into xml file
+            TransformerFactory transformerFactory = TransformerFactory.newInstance();
+            Transformer transformer = transformerFactory.newTransformer();
+            DOMSource source = new DOMSource(doc);
+            StreamResult result = new StreamResult(new File(("cars.xml")));
+            transformer.transform(source, result);
+
+        } catch (Exception e) {
+            e.printStackTrace();
+        }
+    }
+
+
+}
