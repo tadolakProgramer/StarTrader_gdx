340b490675dd7c1f397e2dfd55311ce25150731f
Tadeusz <tadolak@gmail.com>
2018-10-10 11:21:33 +0200

    Remove WindowPlanetInfo from HUD Class

--- core/src/com/mygdx/game/Screenns/Hud/Hud.java
+++ core/src/com/mygdx/game/Screenns/Hud/Hud.java
@@ -1,4 +1,4 @@
-package com.mygdx.game.Screenns;
+package com.mygdx.game.Screenns.Hud;
 
 
 import com.badlogic.gdx.Gdx;
@@ -22,6 +22,7 @@ import com.badlogic.gdx.scenes.scene2d.ui.ProgressBar;
 import com.badlogic.gdx.scenes.scene2d.ui.Skin;
 import com.badlogic.gdx.scenes.scene2d.ui.Table;
 import com.badlogic.gdx.scenes.scene2d.ui.TextButton;
+import com.badlogic.gdx.scenes.scene2d.ui.TextField;
 import com.badlogic.gdx.scenes.scene2d.ui.Window;
 import com.badlogic.gdx.scenes.scene2d.utils.ClickListener;
 import com.badlogic.gdx.scenes.scene2d.utils.TiledDrawable;
@@ -32,16 +33,14 @@ import com.badlogic.gdx.utils.viewport.Viewport;
 
 import com.mygdx.game.GameObjekts.SpaceObjekt.Planet;
 import com.mygdx.game.MyGdxGame;
+import com.mygdx.game.Screenns.GameScreen;
 
 
 import static com.mygdx.game.MyGdxGame.GAME_HEIGHT;
 import static com.mygdx.game.MyGdxGame.GAME_WIDTH;
 
-public class Hud implements Disposable{
+public class Hud extends  AbstractHUD implements Disposable{
 
-    //Scene2D.ui Stage and its own Viewport for HUD
-    public Stage stage;
-    public Viewport viewport;
 
     //Scene2D widgets
     private Label moneyLabel;
@@ -55,33 +54,15 @@ public class Hud implements Disposable{
     private Label grafenPriceLabel;
     private Label woterPriceLabel;
     private Label fuellPriceLabel;
-    private Image planetImage;
-    private Planet planet;
-    private int planetNumber;
+
 
     private float time = 0;
     private int fps = 0;
 
-    public TextureRegion region;
-
-    private Skin skin;
-    private Window window;
-    private int windowInfoPlanetCount = 0;
-    private int windowInfoPlanetMarket = 0;
-    private MyGdxGame game;
-    private GameScreen gameScreen;
 
     public Hud(final GameScreen gameScreen, MyGdxGame game, SpriteBatch sb){
 
-        this.gameScreen = gameScreen;
-        this.game = game;
-        skin = new Skin(Gdx.files.internal("skin/flat-earth-ui.json"));
-
-
-        viewport = new FitViewport(GAME_WIDTH, GAME_HEIGHT, new OrthographicCamera());
-        stage = new Stage(viewport, sb);
-
-        gameScreen.multiplexer.addProcessor(stage);
+        super(gameScreen, game, sb);
 
         //define a table used to organize our hud's labels
         Table table = new Table();
@@ -148,117 +129,9 @@ public class Hud implements Disposable{
 
     public void createWindowPlanetInfo(String planetName) {
 
-        windowInfoPlanetCount++;
+        WindowPlanetInfo windowInfoPlanet = new WindowPlanetInfo(gameScreen, game, sb, planetName);
+        stage.addActor(windowInfoPlanet.window);
 
-        if (windowInfoPlanetCount<=1) {
-
-            gameScreen.multiplexer.addProcessor(stage);
-
-
-            window = new Window(planetName, skin);
-            region = new TextureRegion();
-
-            window.setColor(Color.BLACK);
-            window.getTitleLabel().setColor(Color.CHARTREUSE);
-            window.setSize(400.0f, 400.0f);
-            window.setPosition(GAME_WIDTH / 2.0f-200, GAME_HEIGHT / 2.0f-200);//, Align.center);
-
-            Button button = new Button(skin, "close");
-            window.getTitleTable().add(button).padRight(1.0f);
-            window.setBounds(window.getX(), window.getY(), window.getWidth(),window.getHeight());
-
-            Table winTable = new Table();
-            //winTable.debug();
-            window.add(winTable).grow().pad(10);
-            winTable.align(1);
-            stage.addActor(window);
-            winTable.top();
-
-
-            for (int i = 0; i <= gameScreen.planets.size() - 1; i++) {
-                if (gameScreen.planets.get(i).getSpaceObjectName().equals(planetName)) {
-                    planet = gameScreen.planets.get(i);
-                    planetNumber = i;
-                    region = game.textureAtlas.findRegion(planet.getPath());
-                    planetImage = new Image(game.textureAtlas.findRegion(planet.getPath()));
-
-                    planetImage.setSize(10,10);
-                    titanPriceLabel = new Label(String.format("%.2f", planet.getPriceTitan())+" T$", skin, "titan");
-                    titanPriceLabel.setFontScale(1.5f);
-                    grafenPriceLabel = new Label(String.format("%.2f", planet.getPriceGrafen()), skin, "grafen");
-                    grafenPriceLabel.setFontScale(1.5f);
-                    woterPriceLabel = new Label(String.format("%.2f", planet.getPriceWoter()), skin, "woter");
-                    woterPriceLabel.setFontScale(1.5f);
-                    fuellPriceLabel = new Label(String.format("%.2f", planet.getPriceFuell()), skin, "fuell");
-                    fuellPriceLabel.setFontScale(1.5f);
-                    break;
-                }
-            }
-
-
-            Label titanLabel = new Label("Titan: ", skin, "titan");
-            titanLabel.setFontScale(1.5f);
-
-            Label grafenLabel = new Label("Grafen", skin, "grafen");
-            grafenLabel.setFontScale(1.5f);
-
-            Label woterLabel = new Label("Woter", skin, "woter");
-            woterLabel.setFontScale(1.5f);
-
-            Label fuellLabel = new Label("Fuell", skin, "fuell");
-            fuellLabel.setFontScale(1.5f);
-
-
-            TextButton textButton1 = new TextButton("Start", skin);
-            TextButton textButton2 = new TextButton("Close", skin);
-
-            window.row().pad(5);
-            winTable.row().colspan(2);
-            winTable.add(planetImage).size(100,100);
-
-            winTable.row().pad(5);
-            winTable.add(titanLabel).expandX().left();
-            winTable.add(titanPriceLabel).expandX().right();
-
-            winTable.row().pad(5);
-            winTable.add(grafenLabel).expandX().left();
-            winTable.add(grafenPriceLabel).expandX().right();
-
-
-            winTable.row().pad(5);
-            winTable.add(woterLabel).expandX().left();
-            winTable.add(woterPriceLabel).expandX().right();
-
-            winTable.row().pad(5);
-            winTable.add(fuellLabel).expandX().left();
-            winTable.add(fuellPriceLabel).expand().right();
-
-            winTable.row();
-            winTable.add(textButton1).expand().center().pad(10);
-            winTable.add(textButton2).expand().center().pad(10);
-
-
-            textButton2.addListener(new ClickListener() {
-                @Override
-                public void touchUp(InputEvent event, float x, float y, int pointer, int button) {
-                    window.remove();
-                    windowInfoPlanetCount--;
-                    gameScreen.multiplexer.removeProcessor(stage);
-                }
-            });
-            textButton1.addListener(new ClickListener() {
-                @Override
-                public void  touchUp(InputEvent event, float x, float y, int pointer, int button) {
-                    gameScreen.spaceShipPlayer.setStart(planetNumber);
-                    window.remove();
-                    windowInfoPlanetCount--;
-                    gameScreen.multiplexer.removeProcessor(stage);
-                }
-            });
-        }
-        else{
-            windowInfoPlanetCount--;
-        }
     }
 
     public void createWindowPlanetMarket(String planetName) {
@@ -325,6 +198,8 @@ public class Hud implements Disposable{
             TextButton textButtonClose = new TextButton("Close", skin);
 
             TextButton textButtonBuyTitan =  new TextButton("Buy", skin);
+            TextField textField = new TextField("", skin);
+
             TextButton textButtonSellTitan =  new TextButton("Sell", skin);
 
             TextButton textButtonBuyFuel =  new TextButton("Buy", skin);
@@ -338,6 +213,7 @@ public class Hud implements Disposable{
             winTable.add(titanLabel).expandX().left();
             winTable.add(titanPriceLabel).expandX().right();
             winTable.add(textButtonBuyTitan).left().pad(5);
+            winTable.add(textField).padLeft(10.0f);
             winTable.add(textButtonSellTitan).left().pad(5);
 
             winTable.row().pad(5);
