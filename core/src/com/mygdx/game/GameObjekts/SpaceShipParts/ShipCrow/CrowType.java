172a7c496c8980b32bc631a22ce9b0d062c3ce7a
Tadeusz <tadolak@gmail.com>
2018-10-19 09:57:06 +0200

    correct name crow type

--- core/src/com/mygdx/game/GameObjekts/SpaceShipParts/ShipCrow/CrowType.java
+++ core/src/com/mygdx/game/GameObjekts/SpaceShipParts/ShipCrow/CrowType.java
@@ -1,5 +1,5 @@
 package com.mygdx.game.GameObjekts.SpaceShipParts.ShipCrow;
 
 public enum CrowType {
-    CAPITAN, MECHANIC, ELEKTRONIK, NAVIGATOR, PASSENGER
+    CAPTAIN, MECHANIC, ELEKTRONIK, NAVIGATOR, PASSENGER
 }
