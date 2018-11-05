89d9f174c97fc2d614fd7c79aaf80d18cc7f831d
Tadeusz <tadolak@gmail.com>
2018-11-05 09:12:10 +0100

    remove empty import

--- core/src/com/mygdx/game/GameObjekts/SpaceShipParts/HousingModule.java
+++ core/src/com/mygdx/game/GameObjekts/SpaceShipParts/HousingModule.java
@@ -1,8 +1,5 @@
 package com.mygdx.game.GameObjekts.SpaceShipParts;
 
-import com.badlogic.gdx.graphics.Texture;
-import com.badlogic.gdx.graphics.g2d.Sprite;
-
 public class HousingModule extends ShipModule{
 
 
