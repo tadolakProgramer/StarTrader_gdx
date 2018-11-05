ba7c7aa7a46fb92db4339b69c3a04ce0ddd9ddf3
Tadeusz <tadolak@gmail.com>
2018-11-05 09:09:04 +0100

    remove empty import

--- core/src/com/mygdx/game/GameObjekts/SpaceObjekt/Ware.java
+++ core/src/com/mygdx/game/GameObjekts/SpaceObjekt/Ware.java
@@ -1,5 +1,6 @@
 package com.mygdx.game.GameObjekts.SpaceObjekt;
 
+import com.badlogic.gdx.math.MathUtils;
 import com.mygdx.game.GameObjekts.SpaceShipParts.CargoType;
 
 public class Ware {
@@ -9,11 +10,53 @@ public class Ware {
 
     public Ware(CargoType cargoType) {
         this.cargoType = cargoType;
-        this.price = 1.0;
+        this.price = randomPrice(cargoType);
     }
 
     public CargoType getCargoType() {
         return cargoType;
     }
 
+    public void setPrice (double price){
+        this.price = price;
+    }
+
+    public double randomPrice(CargoType cargoType) {
+
+        double rmin = 0;
+        double rmax = 0;
+
+        switch (cargoType) {
+            case FUEL: {
+                rmin = 5;
+                rmax = 10;
+                break;
+            }
+            case TITAN:{
+                rmin = 5;
+                rmax = 10;
+                break;
+            }
+            case GRAFEN:{
+                rmin = 15;
+                rmax = 20;
+                break;
+            }
+            case WATER:{
+                rmin = 50;
+                rmax = 60;
+                break;
+            }
+            case HELIUM3:{
+                rmin = 500;
+                rmax = 510;
+                break;
+            }
+        }
+        return this.price = MathUtils.random((float) rmin, (float) rmax);
+    }
+
+    public double getPrice() {
+        return price;
+    }
 }
