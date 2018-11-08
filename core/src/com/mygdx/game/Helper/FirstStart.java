5deeebddadd4f5fd88f0a5333e3f24e05cab5bee
Tadeusz <tadolak@gmail.com>
2018-11-08 12:15:21 +0100

    preparation for the first run mode

---
+++ core/src/com/mygdx/game/Helper/FirstStart.java
@@ -0,0 +1,15 @@
+package com.mygdx.game.Helper;
+
+import com.mygdx.game.MyGdxGame;
+import com.mygdx.game.Screenns.CreatePlayerScreen;
+
+public class FirstStart {
+
+    public static boolean CreatePlayer(final MyGdxGame game){
+
+        //game.setScreen(new CreatePlayerScreen(game));
+
+        return true;
+    }
+
+}
