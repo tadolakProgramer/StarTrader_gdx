d5d3e52a1ac0cdfca7b8e004e28f23ba8e033b52
Tadeusz <tadolak@gmail.com>
2018-11-13 14:08:44 +0100

    add window moduleInfo

--- core/src/com/mygdx/game/Screenns/SpaceShipScreen.java
+++ core/src/com/mygdx/game/Screenns/SpaceShipScreen.java
@@ -2,10 +2,14 @@ package com.mygdx.game.Screenns;
 
 
 import com.badlogic.gdx.Gdx;
+import com.badlogic.gdx.scenes.scene2d.InputEvent;
 import com.badlogic.gdx.scenes.scene2d.ui.Label;
 import com.badlogic.gdx.scenes.scene2d.ui.ProgressBar;
 import com.badlogic.gdx.scenes.scene2d.ui.Skin;
 import com.badlogic.gdx.scenes.scene2d.ui.Table;
+import com.badlogic.gdx.scenes.scene2d.ui.TextButton;
+import com.badlogic.gdx.scenes.scene2d.ui.Window;
+import com.badlogic.gdx.scenes.scene2d.utils.ClickListener;
 import com.mygdx.game.GameObjekts.SpaceObjekt.SpaceShipDraft;
 import com.mygdx.game.GameObjekts.SpaceObjekt.SpaceShipPlayer;
 import com.mygdx.game.GameObjekts.SpaceShipParts.CargoType;
@@ -13,12 +17,15 @@ import com.mygdx.game.GameObjekts.SpaceShipParts.ModuleActor;
 import com.mygdx.game.GameObjekts.SpaceShipParts.ModuleType;
 import com.mygdx.game.GameObjekts.SpaceShipParts.ShipModule;
 import com.mygdx.game.MyGdxGame;
+import com.mygdx.game.Screenns.Hud.WindowPaymentList;
 
 import java.util.ArrayList;
 import java.util.List;
 
-import static com.mygdx.game.GameObjekts.SpaceShipParts.ModuleType.EMPTY;
 import static com.mygdx.game.MyGdxGame.FILE_EARTH_SKIN;
+import static com.mygdx.game.MyGdxGame.FILE_UI_SKIN;
+import static com.mygdx.game.MyGdxGame.GAME_HEIGHT;
+import static com.mygdx.game.MyGdxGame.GAME_WIDTH;
 
 public class SpaceShipScreen extends AbstractScreen {
 
@@ -37,12 +44,15 @@ public class SpaceShipScreen extends AbstractScreen {
     private Label fuelLabelText;
     private ProgressBar fuelFillBar;
     protected Skin skin;
+    private Skin skin2;
     private Table table;
     private Table tableCrows;
 
     private List<Table> crowTables = new ArrayList<>();
     private Label waterLabelText;
     private Label waterlLabel;
+
+    private Window windowShipModuleInfo;
 
 
     public SpaceShipScreen(final MyGdxGame game, final SpaceShipPlayer spaceShipPlayer) {
@@ -53,6 +63,7 @@ public class SpaceShipScreen extends AbstractScreen {
         tableCrows = new Table();
         isVisible = true;
         skin = new Skin(Gdx.files.internal(FILE_EARTH_SKIN));
+        skin2 = new Skin(Gdx.files.internal(FILE_UI_SKIN));
         intSpaceShipDraft(spaceShipPlayer);
         createTableWithCargo();
         createTableCrows();
@@ -65,7 +76,7 @@ public class SpaceShipScreen extends AbstractScreen {
 
                 int index = i;
                 ShipModule shipModule = spaceShipPlayer.shipModules.get(i);
-                moduleActor = new ModuleActor(game, shipModule , index, spaceShipDraft.getScale());
+                moduleActor = new ModuleActor(this, shipModule , index, spaceShipDraft.getScale());
                 stage.addActor(spaceShipDraft);
                 stage.addActor(moduleActor);
 
@@ -205,6 +216,67 @@ public class SpaceShipScreen extends AbstractScreen {
 
         //stage.addActor(fuelFillBar);
 
+    }
 
+    public void createWindowShipModuleInfo(ShipModule shipModule){
+
+
+        int wares = shipModule.capacitys.size();
+        windowShipModuleInfo = new Window(shipModule.getModuleType().name(), skin2);
+        TextButton btnClose = new TextButton("Close", skin2);
+        Table table = new Table();
+
+        table.align(1);
+        table.top();
+
+        table.row();
+        table.add(new Label("Name: ", skin2)).pad(5);
+        table.add(new Label(shipModule.name, skin2)).pad(5);
+
+        table.row();
+        table.add(new Label("Capacity: ", skin2)).pad(5);
+        table.add(new Label(""+shipModule.getCapacity(), skin2)).pad(5);
+
+        for (int i =0; i < CargoType.values().length; i++){
+            if (shipModule.capacitys.containsKey(CargoType.values()[i])){
+                table.row();
+                table.add(new Label((CargoType.values()[i]).toString(), skin2)).pad(5);
+                table.add(new Label(shipModule.capacitys.get(CargoType.values()[i]).toString(), skin2)).pad(5);
+                //
+            }
+        }
+
+        table.row();
+        table.add(new Label("Fill: ", skin2)).pad(5);
+        table.add(new Label(String.format("%.2f",shipModule.getFill()), skin2)).pad(5);
+
+        table.row();
+        table.add(new Label("Base failure rate: ", skin2)).pad(5);
+        table.add(new Label(""+shipModule.baseFailureRate, skin2)).pad(5);
+
+        table.row();
+        table.add(new Label("Factor failure rate: ", skin2)).pad(5);
+        table.add(new Label(""+shipModule.experienceLevel, skin2)).pad(5);
+
+        table.row();
+        table.add(new Label("Cost: ", skin2)).pad(5);
+        table.add(new Label(""+shipModule.cost, skin2)).pad(5);
+
+        table.row().colspan(2).pad(10);
+        table.add(btnClose);
+
+        windowShipModuleInfo.setSize(300,400);
+        windowShipModuleInfo.setPosition(GAME_WIDTH / 2.0f - 100, GAME_HEIGHT / 2.0f - 100);
+        windowShipModuleInfo.add(table).grow().pad(10);
+
+        stage.addActor(windowShipModuleInfo);
+
+        btnClose.addListener(new ClickListener() {
+            @Override
+            public void touchUp(InputEvent event, float x, float y, int pointer, int button) {
+                windowShipModuleInfo.remove();
+                //gameScreen.multiplexer.removeProcessor(stage);
+            }
+        });
     }
 }
