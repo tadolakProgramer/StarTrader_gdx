c0d2b400fcf40deb2f19b3a328b2d172255b69b2
Tadeusz <tadolak@gmail.com>
2018-11-14 13:25:29 +0100

    add method change price

--- core/src/com/mygdx/game/GameObjekts/SpaceObjekt/Ware.java
+++ core/src/com/mygdx/game/GameObjekts/SpaceObjekt/Ware.java
@@ -26,43 +26,45 @@ public class Ware {
         this.price = price;
     }
 
+    public double changePrice(){
+        return this.price * MathUtils.random(-1.049f, 1.051f);
+    }
+
     public double randomPrice(CargoType cargoType) {
 
         double rmin = 0;
-        double rmax = 0;
 
         switch (cargoType) {
             case FUEL: {
                 rmin = 5;
-                rmax = 10;
                 break;
             }
             case TITAN:{
                 rmin = 5;
-                rmax = 10;
                 break;
             }
             case GRAFEN:{
                 rmin = 15;
-                rmax = 20;
                 break;
             }
             case WATER:{
                 rmin = 50;
-                rmax = 60;
                 break;
             }
             case HELIUM3:{
                 rmin = 500;
-                rmax = 510;
                 break;
             }
             case PERSON:{
                 rmin = 5000;
-                rmax = 5500;
+                break;
+            }
+            case QUICKSILVER:{
+                rmin = 1000;
+                break;
             }
         }
-        return this.price = MathUtils.random((float) rmin, (float) rmax);
+        return this.price = MathUtils.random((float) rmin, (float) rmin * 1.2f);
     }
 
     public double getPrice() {
