9a2725dd032cca8824e234bf40e759afe81fe2e6
Tadeusz <tadolak@gmail.com>
2018-11-15 11:05:13 +0100

    fix constructor

--- core/src/com/mygdx/game/GameObjekts/SpaceShipParts/SpaceShipCocpit.java
+++ core/src/com/mygdx/game/GameObjekts/SpaceShipParts/SpaceShipCocpit.java
@@ -5,8 +5,6 @@ public class SpaceShipCocpit extends ShipModule{
     public SpaceShipCocpit(ModuleType moduleType, String name, double capacity, double cost, int index, int baseFailureRate){
         super(moduleType, name, capacity, cost, index);
 
-
-        this.capacity = 2.0;
         this.fill = fill;
         this.baseFailureRate = baseFailureRate;
 
