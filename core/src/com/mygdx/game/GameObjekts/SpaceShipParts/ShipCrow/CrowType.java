6474cb5b08ffd9f3c424dc2723e9e0e3ea10a472
Tadeusz <tadolak@gmail.com>
2018-11-19 14:01:38 +0100

    remove PASSENGER

--- core/src/com/mygdx/game/GameObjekts/SpaceShipParts/ShipCrow/CrowType.java
+++ core/src/com/mygdx/game/GameObjekts/SpaceShipParts/ShipCrow/CrowType.java
@@ -1,5 +1,5 @@
 package com.mygdx.game.GameObjekts.SpaceShipParts.ShipCrow;
 
 public enum CrowType {
-    CAPTAIN, MECHANIC, ELEKTRONIK, NAVIGATOR, PASSENGER, PLUMBER, BIOLOGIST
+    CAPTAIN, MECHANIC, ELEKTRONIK, NAVIGATOR, PLUMBER, BIOLOGIST
 }
